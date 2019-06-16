db60eb8f72fc3a811078b0f869e8165fc4c5ba2a
Jason Leach <jlspursfan@gmail.com>
2019-06-16 21:19:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,17 +2,18 @@ require 'pry'
 
 class Song
 
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
 
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  #@@songs = []
 
   def initialize
-    @@songs << self
+    self.class.all << self
   end
 
   def self.all
