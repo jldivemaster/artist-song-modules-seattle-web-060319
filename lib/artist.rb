bcc9abcfca6f0ef8324197c70bbc2fe3ee77bf72
Jason Leach <jlspursfan@gmail.com>
2019-06-16 21:01:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
 
   extend Memorable
-  
+
   attr_accessor :name
   attr_reader :songs
 
@@ -21,14 +21,6 @@ class Artist
   def self.all
     @@artists
   end
-
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
 
   def add_song(song)
     @songs << song
